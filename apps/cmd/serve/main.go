c5285a098fc35b86bb6140cad09284e7af90aad9
zhang <nanfeng.dev@gmail.com>
2026-01-19 17:14:26 +0800

    feat: 数据库连接

--- apps/cmd/serve/main.go
+++ apps/cmd/serve/main.go
@@ -5,13 +5,21 @@ import (
 	"log"
 
 	"mini-blog/internal/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 // main 程序的主入口
 func main() {
 	// 1.读取配置文件
-	_, err := config.ConfigurationInit()
+	cfg, err := config.ConfigurationInit()
 	if err != nil {
 		log.Fatal("error: %w", err)
+	}
+	// 2.连接数据库
+	_, err = gorm.Open(postgres.Open(cfg.Datasource.Postgres.Dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("数据库连接错误: %w", err)
 	}
 }
